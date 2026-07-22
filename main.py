a122e64aacd53d1092de93419b95248c4bc69d84
BENJAMIN IGNACIO LEMMERMANN SOTO <be.lemmermann@duocuc.cl>
2026-07-22 04:39:35 -0400

    feat: opcion 1 - busqueda por plataforma compartida + buscar juego

--- main.py
+++ main.py
@@ -33,6 +33,23 @@ def leer_opcion(min , max):
     except ValueError:
         print("Debes ingresar datos numericos")
 
+def buscar_juego(lista_juegos, titulo): 
+    for juego in lista_juegos: 
+        if juego["titulo"].upper() == titulo.upper(): 
+            return True 
+    else:
+        return False
+
+def buscar_plataforma(biblioteca, plataforma):
+    lista = []
+    for clave, valor in biblioteca.items(): 
+        usuario = clave
+        lista_juegos = valor 
+        for juego in lista_juegos:
+            if juego["plataforma"].upper() == plataforma.upper():
+                titulo = juego["titulo"]
+                lista.append(f"{titulo} -- Dueño del perfil: {usuario}")
+    return lista
 
 def main():
 
@@ -43,7 +60,14 @@ def main():
 
         opcion = leer_opcion(1,5)
         if opcion == 1:
-            pass
+            plataforma = input("Ingrese el nombre de la plataforma a buscar: ")
+            lista = buscar_plataforma(biblioteca, plataforma)
+            if len(lista) == 0:
+                print("No hay juegos para esta plataforma") 
+            else:
+                print(f"Juegos encontrados en {plataforma}")
+                for i in lista:
+                    print(i)
         elif opcion == 2:
             pass
         elif opcion == 2:
